2248acbd308844868fab56b8c94b84603562a6f0
LunarcatOwO <54731257+LunarcatOwO@users.noreply.github.com>
2025-03-07 20:35:33 -0500

    Deleted scale embed

--- src/utils/scaling.js
+++
@@ -1,8 +0,0 @@
-export function scaleEmbed(originalWidth, originalHeight, requestedWidth) {
-    const aspectRatio = originalWidth / originalHeight;
-    const scaledHeight = requestedWidth / aspectRatio;
-    return {
-        width: requestedWidth,
-        height: scaledHeight
-    };
-}
