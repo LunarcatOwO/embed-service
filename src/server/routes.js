94ce56dd1af43e070f5cdd5d9497eb999b3efe3e
Kate Bodbyl <150171084+Shotet13@users.noreply.github.com>
2025-03-31 18:10:03 -0400

    refactor: ensure full-width layout for body and html in routes setup

--- src/server/routes.js
+++ src/server/routes.js
@@ -292,6 +292,7 @@ function setupRoutes(app) {
         body, html { 
             margin: 0; 
             padding: 0; 
+            width: 100%; 
             height: 100%; 
             display: flex; 
             justify-content: center; 
